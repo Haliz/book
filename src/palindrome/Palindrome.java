bee23bac9c24332bf7a0c78182faa206a800040f
Haliz <foril2n1@rambler.ru>
2021-01-19 22:25:55 +0300

    Палиндром. Суммирование цифр числа

--- src/palindrome/Palindrome.java
+++ src/palindrome/Palindrome.java
@@ -6,13 +6,19 @@ public class Palindrome {
         System.out.println(isPalindrome("12345"));
         System.out.println(isPalindrome("123321"));
 
-//        System.out.println(isSumEven("21345"));
-//        System.out.println(isSumEven("999"));
+        System.out.println(isSumEven("21345"));
+        System.out.println(isSumEven("999"));
     }
 
-    private static boolean isSumEven(String numberAsString) {
-        //write you code here
-        return false;
+    private static int isSumEven(String numberAsString) {
+       var n = Integer.parseInt(numberAsString);
+        int sum = 0;
+        while (n != 0) {
+            //Суммирование цифр числа
+            sum += (n % 10);
+            n /= 10;
+        }
+        return sum;
     }
 
     private static boolean isPalindrome(String numberAsString) {
