4c17b3a9b71835405b3f5f4f6b2f7ea06ff59c7e
Haliz <foril2n1@rambler.ru>
2021-02-07 19:24:58 +0300

    1.1. Получение компаратора. Метод naturalOrder()

---
+++ src/practice/sorted/NaturalOrder.java
@@ -0,0 +1,10 @@
+package practice.sorted;
+
+import java.util.Comparator;
+
+public class NaturalOrder {
+    public static Comparator<String> naturalOrder() {
+        return Comparator.naturalOrder();
+    }
+}
+
