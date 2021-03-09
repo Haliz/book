0fae3faafa98cb813fb5153c1ac40a43c69bcf39
Haliz <foril2n1@rambler.ru>
2021-03-09 21:34:46 +0300

    1.1.5. Циклы  2. Вывести на консоль числа от N до M

---
+++ src/practice/cicle/PrintNToM.java
@@ -0,0 +1,9 @@
+package practice.cicle;
+
+public class PrintNToM {
+    public static void out(int n, int m) {
+        for (int a = n; a < m; a++) {
+            System.out.println(a);
+        }
+    }
+}
