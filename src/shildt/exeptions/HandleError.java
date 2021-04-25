f4440b264ffd5d39f3a2aa358f7408a1808ac999
Haliz <foril2n1@rambler.ru>
2021-04-25 10:56:19 +0300

    Шилдт. Исключения

---
+++ src/shildt/exeptions/HandleError.java
@@ -0,0 +1,22 @@
+package shildt.exeptions;
+
+import java.util.Random;
+
+public class HandleError {
+    public static void main(String[] args) {
+        int a = 0, b = 0, c = 0;
+        Random r = new Random();
+        for (int i = 0; i < 50; i++) {
+            try {
+                b = r.nextInt();
+                c = r.nextInt();
+                a = 12345 / (b / c);
+            } catch (ArithmeticException e) {
+                System.out.println("Деление на нуль " + e);
+//                e.printStackTrace();
+                a = 0;
+            }
+            System.out.println("a: " + a);
+        }
+    }
+}
