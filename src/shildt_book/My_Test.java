8658385eecba77281266e752e101dd4146777a97
Haliz <foril2n1@rambler.ru>
2021-07-29 15:55:27 +0300

    Мой тест

---
+++ src/shildt_book/My_Test.java
@@ -0,0 +1,14 @@
+package shildt_book;
+
+import java.io.Console;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+public class My_Test {
+    public static void main(String[] args) throws IOException {
+        Console console = System.console();
+        System.out.println(console);
+    }
+}
