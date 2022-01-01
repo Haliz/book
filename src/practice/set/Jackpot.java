ae4e855935adc97bc7182f56d54f78b266c6fc6c
garry <foril2n1@rambler.ru>
2022-01-01 15:28:30 +0300

    0.5. Джекпот в казино (2 часть)

--- src/practice/set/Jackpot.java
+++ src/practice/set/Jackpot.java
@@ -1,4 +1,17 @@
 package practice.set;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Jackpot {
+    public static boolean checkYourWin(String[] combination) {
+        Set<String> strings = new HashSet<>();
+        for (int i = 0; i < combination.length; i++) {
+            strings.add(combination[i]);
+        }
+        if (strings.size() == 1) {
+            return true;
+        }
+        return false;
+    }
 }
