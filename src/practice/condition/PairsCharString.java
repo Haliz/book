08acfb578074a548cef312e50e97809a873b92d9
Haliz <foril2n1@rambler.ru>
2022-05-10 23:39:08 +0300

    1.8. Необычные пары

---
+++ src/practice/condition/PairsCharString.java
@@ -0,0 +1,8 @@
+package practice.condition;
+
+public class PairsCharString {
+    public static boolean check(String l, String r) {
+        return (l.isEmpty() && r.isEmpty())
+                || (l.charAt(0) == r.charAt(r.length()-1) && r.charAt(0) == l.charAt(l.length()-1));
+    }
+}
