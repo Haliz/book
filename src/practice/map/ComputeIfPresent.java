a4bf5971518cc57c8e0325733c4c955c98dc93d1
Haliz <foril2n1@rambler.ru>
2022-10-11 21:53:10 +0300

    5. Изменение уже существующей записи в отображении Рефакторинг.

--- src/practice/map/ComputeIfPresent.java
+++ src/practice/map/ComputeIfPresent.java
@@ -2,11 +2,13 @@ package practice.map;
 
 import java.util.Map;
 
+
 public class ComputeIfPresent {
     public static Map<Integer, String> collectData(
             Map<Integer, String> name, Map<Integer, String> surname) {
-        name.computeIfPresent(1, (key, value) -> value + " " + surname.get(key));
-        name.computeIfPresent(2, (key, value) -> value + " " + surname.get(key));
+      for (Map.Entry<Integer, String>  k : name.entrySet()) {
+        name.computeIfPresent(k.getKey(), (key, value) -> value + " " + surname.get(key));
+      }
         return name ;
     }
 }
