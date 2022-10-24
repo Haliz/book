f599c91ddcbd020a204d5e940a710ed06ea767d2
Haliz <foril2n1@rambler.ru>
2022-10-24 21:33:34 +0300

    6. Вычисление значений в отображении если ключа не существует

---
+++ src/practice/map/ComputeIfAbsentTest.java
@@ -0,0 +1,28 @@
+package practice.map;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+import static practice.map.ComputeIfAbsent.User;
+
+public class ComputeIfAbsentTest {
+    @Test
+    public void collectData() {
+        Map<Integer, String> names = new HashMap<>(Map.of(1, "", 2, ""));
+        List<ComputeIfAbsent.User> users = List.of(
+                new ComputeIfAbsent.User(1, "name1"),
+                new User(2, "name2"),
+                new User(3, "name3"),
+                new User(4, "name4")
+        );
+        Map<Integer, String> rsl = ComputeIfAbsent.collectData(names, users);
+        Map<Integer, String> expected = Map.of(4, "name4", 1, "name1", 2, "name2", 3, "name3");
+        assertThat(rsl, is(expected));
+    }
+}
